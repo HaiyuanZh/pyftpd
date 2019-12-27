014c5baec23b2f751b6495fad382d2e2813dea11
Haiyuan <zhanghaiyuan@gmail.com>
2019-12-27 21:17:38 +0800

    基于Python的简单 ftp服务器

---
+++ pyftpd.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+
+    # Define a new user having full r/w permissions and a read-only
+    # anonymous user
+    # authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+    # authorizer.add_anonymous(os.getcwd())
+    authorizer.add_anonymous("/Users/Haiyuan/vmshare/ftproot", perm='elradfmwMT')
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    # Specify a masquerade address and the range of ports to use for
+    # passive connections.  Decomment in case you're behind a NAT.
+    #handler.masquerade_address = '151.25.42.11'
+    #handler.passive_ports = range(60000, 65535)
+    # 外部公网地址
+    # handler.masquerade_address = '183.220.1.161'
+    # 需要在外网路由器设置端口映射
+    # handler.passive_ports = range(60000, 61000)
+
+    # Instantiate FTP server class and listen on 0.0.0.0:2121
+    address = ('', 2121)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
