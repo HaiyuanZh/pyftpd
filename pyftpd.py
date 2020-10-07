b68ccbd4e563d384dc455167835b6a0e79a4e8ae
Haiyuan <zhanghaiyuan@gmail.com>
2020-10-07 11:39:55 +0800

    由于在Mac总是提示not responding，修改为有界面方式

--- pyftpd.py
+++ pyftpd.py
@@ -1,13 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+# import os
+import sys
+import tkinter as tk
+import tkinter.font as tkFont
+import threading
 
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
+from pyftpdlib.servers import ThreadedFTPServer   ##多线程
+# from pyftpdlib.servers import MultiprocessFTPServer  ##多进程
+# from pyftpdlib.servers import FTPServer
 
-def main():
+class MyHandler(FTPHandler):
+
+    def set_label(self, label):
+        self.label = label
+        pass
+
+    def on_connect(self):
+        self.remote_ip_str = self.remote_ip.split(':')[-1]
+        str = "%s connected" % self.remote_ip_str
+        self.label.config(text = str)
+        pass
+
+    def on_disconnect(self):
+        str = "%s disconnected" % self.remote_ip_str
+        # do something when client disconnects
+        self.label.config(text = str)
+        pass
+
+    def on_login(self, username):
+        # do something when user login
+        self.label.config(text="%s %s用户登入" % (self.remote_ip_str, username))
+        pass
+
+    def on_logout(self, username):
+        self.label.config(text="%s 用户%s登出" % (self.remote_ip_str, username))
+        # do something when user logs out
+        pass
+
+    def on_file_sent(self, file):
+        # do something when a file has been sent
+        self.label.config(text="%s 文件发送" % self.remote_ip_str)
+        pass
+
+    def on_file_received(self, file):
+        # do something when a file has been received
+        self.label.config(text="%s 文件收到" % self.remote_ip_str)
+        pass
+
+    def on_incomplete_file_sent(self, file):
+        self.label.config(text="%s 完成文件发送" % self.remote_ip_str)
+        # do something when a file is partially sent
+        pass
+
+    def on_incomplete_file_received(self, file):
+        self.label.config(text="%s 完成文件接收" % self.remote_ip_str)
+        # remove partially uploaded files
+        import os
+        os.remove(file)
+
+def ftpd(showlabel):
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = DummyAuthorizer()
 
@@ -15,10 +70,11 @@ def main():
     # anonymous user
     # authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
     # authorizer.add_anonymous(os.getcwd())
-    authorizer.add_anonymous("/Users/Haiyuan/vmshare/ftproot", perm='elradfmwMT')
+    authorizer.add_anonymous("/Users/Haiyuan/vmshare", perm='elradfmwMT')
 
     # Instantiate FTP handler class
-    handler = FTPHandler
+    handler = MyHandler
+    handler.set_label(handler, showlabel)
     handler.authorizer = authorizer
 
     # Define a customized banner (string returned when client connects)
@@ -35,14 +91,77 @@ def main():
 
     # Instantiate FTP server class and listen on 0.0.0.0:2121
     address = ('', 2121)
-    server = FTPServer(address, handler)
-
+    # server = FTPServer(address, handler)
+    server = ThreadedFTPServer(address, handler)
+    # server = MultiprocessFTPServer(address, handler)
     # set a limit for connections
     server.max_cons = 256
     server.max_cons_per_ip = 5
 
     # start ftp server
-    server.serve_forever()
+    # server.serve_forever()
+    srv = threading.Thread(target=server.serve_forever)
+    srv.daemon = True
+    srv.start()
+
+def btnExit():
+    sys.exit()
+
 
 if __name__ == '__main__':
-    main()
+
+    root = tk.Tk()
+    root.geometry("900x600")
+    root.title("Awooa出品的自用ftp服务端工具")
+    label = tk.Label(root, text='提供ftp服务给外部系统',
+                     fg='#0000CD',
+                     pady=200,
+                     font=tkFont.Font(size=40))
+
+    ftpd(label)
+    label.pack()
+
+    pixelVirtual = tk.PhotoImage(width=1, height=1)
+    tk.Button(root, text='退 出',font=tkFont.Font(size=30),
+              foreground='green',
+              background='black',
+              image=pixelVirtual,
+              width=200,
+              height=80,
+              compound=tk.CENTER,
+              command=btnExit).pack()
+    tk.mainloop()
+
+# class Application(tk.Frame):
+#     def __init__(self, master=None):
+#         super().__init__(master)
+#         master.geometry("640x480")
+#         master.columnconfigure(0, weight = 0)
+#         master.rowconfigure(2, weight = 0)
+#         self.master = master
+#         self.pack()
+#         self.create_widgets()
+#
+#     def create_widgets(self):
+#         self.hi_there = tk.Button(self, bg="blue")
+#         self.hi_there["text"] = "启动ftpd"
+#         self.hi_there["command"] = self.onclick
+#         # self.hi_there.configure(width=100,height=50)
+#         self.hi_there.pack(side="top")
+#         # self.hi_there.grid(row=0,column =0)
+#
+#         self.quit = tk.Button(self, text="QUIT", fg="red",
+#                               command=self.master.destroy)
+#         # self.quit.grid(row = 1, column = 0)
+#         # self.quit.configure(width=100,height=50)
+#         self.quit.pack(side="bottom")
+#         # self.quit.grid(row=1,column=0)
+#
+#     def onclick(self):
+#         self.hi_there["state"] = "disabled"
+#         ftpd()
+#
+# if __name__ == '__main__':
+#     root = tk.Tk()
+#     app = Application(master=root)
+#     app.mainloop()
--- readme.md
+++ readme.md
@@ -1,8 +1,17 @@
 ## 说明
 
 只增加了匿名用户，允许写入
-默认目录在 /Users/Haiyuan/vmshare/ftproot
+默认目录在 /Users/Haiyuan/vmshare
 默认端口 2121
+
+
+## py2app生成独立应用
+$ rm -rf build dist
+$ python setup.py py2app
+
+#问题
+发现有# import tkinter as tk 这个以后
+做成的app，无法运行，暂时去掉
 
 
 ## 使用 sslforfree.com 生成证书
@@ -10,3 +19,4 @@
 ## 折腾了半天，完全没有用
 因为运营商IP，封杀了80端口，因此let's Encrypt完全无法验证。
 只能临时把域名指到外部vps，在用nginx申请，申请之后，再指回来
+
